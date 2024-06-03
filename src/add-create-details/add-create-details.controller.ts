dc0bf35c557b7eecfa1daeb9dfec4929c4819bad
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2024-06-04 00:47:19 +0530

    updated

--- src/add-create-details/add-create-details.controller.ts
+++ src/add-create-details/add-create-details.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors } from '@nestjs/common';
 import { AddCreateDetailsService } from './add-create-details.service';
 import { CreateAddCreateDetailDto } from './dto/create-add-create-detail.dto';
 import { UpdateAddCreateDetailDto } from './dto/update-add-create-detail.dto';
 import { postDetails } from './schemas/add-create-details.schema';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('add-create-details')
 export class AddCreateDetailsController {
@@ -11,6 +12,7 @@ export class AddCreateDetailsController {
    ){}
 
    @Post('post-details')
+   @UseInterceptors(FileInterceptor('file'))
    async create(@Body() createAddCreateDetailDto: postDetails) {
     console.log(createAddCreateDetailDto);
     
